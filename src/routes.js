17d46e43df6568f863eb7ee9c8f511a6beafe4c6
Artem Yashin <artemyashindev@gmail.com>
2023-03-13 21:10:21 +0600

    Safety update

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,13 @@ export const adminRoutes = [
   '/article/edit/:id',
   '/api/settings/save',
   '/api/settings/password',
+  '/api/settings/logo',
+  '/api/settings/watermark',
+  '/api/article/delete',
+  '/api/article/image',
+  '/api/article/new',
+  '/api/article/reorder',
+  '/api/article/update',
 ];
 
 export const protectedRoutes = [
